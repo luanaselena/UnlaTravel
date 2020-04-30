c5f167b83597b19e0be9aacd7d3da2b80d756d9e
luanaselena <52636725+luanaselena@users.noreply.github.com>
2020-04-30 18:35:27 -0300

    creacion de la estructura del proyecto

---
+++ src/main/java/com/unla/travelweb/TravelwebApplication.java
@@ -0,0 +1,13 @@
+package com.unla.travelweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelwebApplication.class, args);
+	}
+
+}
