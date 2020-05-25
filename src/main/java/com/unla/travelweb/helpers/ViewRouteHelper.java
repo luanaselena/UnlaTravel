db026b2dbc379cedadadb66c14efdd9472e75272
luanaselena <52636725+luanaselena@users.noreply.github.com>
2020-05-25 04:53:12 -0300

    carousel con imagenes

--- src/main/java/com/unla/travelweb/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/travelweb/helpers/ViewRouteHelper.java
@@ -2,23 +2,23 @@ package com.unla.travelweb.helpers;
 
 public class ViewRouteHelper {
 
-	
-	public final static String INDEX = "home/index";
-	public final static String HELLO = "home/hello";
-	
-	public final static String USER_LOGIN = "user/login";
-	public final static String USER_LOGOUT = "user/logout";
-	
+
+    public final static String INDEX = "home/index";
+    public final static String HELLO = "home/hello";
+
+    public final static String USER_LOGIN = "user/login";
+    public final static String USER_LOGOUT = "user/logout";
+
     public final static String USUARIO_INDEX = "usuario/index";
     public final static String USUARIO_NEW = "usuario/new";
     public final static String USUARIO_UPDATE = "usuario/update";
     public final static String USUARIO_ROOT = "/usuario";
-    
+
     public final static String HOTEL_INDEX = "hotel/index";
     public final static String HOTEL_NEW = "hotel/new";
     public final static String HOTEL_UPDATE = "hotel/update";
     public final static String HOTEL_ROOT = "/hotel";
-    
+
     public final static String VUELO_INDEX = "vuelo/index";
     public final static String VUELO_NEW = "vuelo/new";
     public final static String VUELO_UPDATE = "vuelo/update";
@@ -29,7 +29,7 @@ public class ViewRouteHelper {
     public final static String DESTINO_NEW = "destino/new";
     public final static String DESTINO_UPDATE = "destino/update";
     public final static String DESTINO_ROOT = "/destino";
-    
+
     public final static String ACTIVIDAD_INDEX = "actividad/index";
     public final static String ACTIVIDAD_NEW = "actividad/new";
     public final static String ACTIVIDAD_UPDATE = "actividad/update";
@@ -39,13 +39,9 @@ public class ViewRouteHelper {
     public final static String PAQUETE_NEW = "paquete/new";
     public final static String PAQUETE_UPDATE = "paquete/update";
     public final static String PAQUETE_ROOT = "/paquete";
-<<<<<<< HEAD
-    
+
     public final static String CARRITO_INDEX = "carrito/index";
     public final static String CARRITO_NEW = "carrito/new";
     public final static String CARRITO_UPDATE = "carrito/update";
     public final static String CARRITO_ROOT = "/carrito";
-    
-=======
->>>>>>> d44739d7667256ef82adba8e8ae258e1a6cbd229
 }
